93864b1b3085650fc0217c1960ddc97632e66a8d
johnraphael02 <john.c@strastan.com>
2025-09-19 11:13:43 +0800

    Resolved issues in AuthWrapper.tsx

--- frontend/my-app/components/AuthWrapper.tsx
+++ frontend/my-app/components/AuthWrapper.tsx
@@ -1,7 +1,16 @@
-"use client";
-import { oidcConfigSignInConfig } from "@/src/utils/oidcConfigSignInConfig";
-import { AuthProvider } from "react-oidc-context";
+// "use client";
+// import { AuthProvider } from "react-oidc-context";
+// import { oidcConfigSignInConfig } from "../src/utils/oidcConfigSignInConfig";
 
-export default function AuthWrapper({ children }) {
+// export default function AuthWrapper({ children }) {
+//   return <AuthProvider {...oidcConfigSignInConfig}>{children}</AuthProvider>;
+// }
+
+//(2)
+"use client";
+import { AuthProvider } from "react-oidc-context";
+import { oidcConfigSignInConfig } from "../src/utils/oidcConfigSignInConfig";
+
+export default function AuthWrapper({ children }: { children: any }) {
   return <AuthProvider {...oidcConfigSignInConfig}>{children}</AuthProvider>;
 }
