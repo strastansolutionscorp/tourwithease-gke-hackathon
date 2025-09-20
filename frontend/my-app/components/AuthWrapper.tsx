187e7e834197584fbcad891376b5f09f33b84f81
johnraphael02 <john.c@strastan.com>
2025-09-20 19:51:46 +0800

    Fixed the configuration in oidcConfigSigninConfig.js

--- frontend/my-app/components/AuthWrapper.tsx
+++ frontend/my-app/components/AuthWrapper.tsx
@@ -7,10 +7,25 @@
 // }
 
 //(2)
+// "use client";
+// import { AuthProvider } from "react-oidc-context";
+// import { oidcConfigSignInConfig } from "../src/utils/oidcConfigSigninConfig.js";
+
+// export default function AuthWrapper({ children }: { children: any }) {
+//   return <AuthProvider {...oidcConfigSignInConfig}>{children}</AuthProvider>;
+// }
+
+//(3)
 "use client";
 import { AuthProvider } from "react-oidc-context";
 import { oidcConfigSignInConfig } from "../src/utils/oidcConfigSigninConfig.js";
 
 export default function AuthWrapper({ children }: { children: any }) {
-  return <AuthProvider {...oidcConfigSignInConfig}>{children}</AuthProvider>;
+  const config = {
+    ...oidcConfigSignInConfig,
+    authority: oidcConfigSignInConfig.authority!,
+    client_id: oidcConfigSignInConfig.client_id!,
+  };
+
+  return <AuthProvider {...config}>{children}</AuthProvider>;
 }
